e2de8ee824193c71e4484a84f7178a150f757431
Abulfazfa <abulfazfa@code.edu.az>
2024-10-26 21:29:50 +0400

    utils/redis

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,39 @@
 module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
-    },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
-    ],
-    globals: {
+  env: {
+      browser: false, // Set to false if your code does not run in a browser
+      node: true, // Enable Node.js global variables
+      es6: true, // Enable ES6 syntax
+      jest: true, // If you're using Jest for testing
+  },
+  extends: [
+      'airbnb-base', // Extend Airbnb base rules
+      'plugin:jest/all', // Extend Jest rules
+      'eslint:recommended', // Extend recommended ESLint rules
+  ],
+  globals: {
       Atomics: 'readonly',
       SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: ['jest'],
-    rules: {
-      'max-classes-per-file': 'off',
-      'no-underscore-dangle': 'off',
-      'no-console': 'off',
-      'no-shadow': 'off',
+  },
+  parserOptions: {
+      ecmaVersion: 2018, // Set the ECMAScript version
+      sourceType: 'module', // Allow ES module syntax
+  },
+  plugins: ['jest'], // Include the Jest plugin
+  rules: {
+      'max-classes-per-file': 'off', // Allow multiple classes per file
+      'no-underscore-dangle': 'off', // Disable no-underscore-dangle rule
+      'no-console': 'off', // Allow console statements
+      'no-shadow': 'off', // Disable no-shadow rule
       'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
+          'error',
+          'LabeledStatement',
+          'WithStatement', // Disallow specific syntax
       ],
-    },
-    overrides:[
+  },
+  overrides: [
       {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
+          files: ['*.js'],
+          excludedFiles: 'babel.config.js', // Exclude babel.config.js from this configuration
+      },
+  ],
 };
